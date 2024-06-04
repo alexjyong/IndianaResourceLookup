b54b0a5df02d503bd90123f9d2dceb4551c668d5
Alex Yong <alexjyong@gmail.com>
2024-06-04 13:55:45 -0400

    Update map.py

--- map.py
+++ map.py
@@ -1,8 +1,8 @@
 import googlemaps
 import json
 
-# Replace with your Google Maps API key
-gmaps = googlemaps.Client(key='YOUR_API_KEY') #yes this should be an environment file. don't judge me.
+# Replace with your Google Maps API key 
+gmaps = googlemaps.Client(key='YOUR_API_KEY') #yes this should be an env file. don't judge me
 
 indiana_counties = [
     "Adams", "Allen", "Bartholomew", "Benton", "Blackford", "Boone", "Brown",
@@ -23,6 +23,20 @@ indiana_counties = [
 ]
 
 all_township_data = []
+
+def replace_unicode_spaces(data):
+  """Recursively replaces Unicode spaces in strings within a data structure."""
+  if isinstance(data, dict):
+    return {key: replace_unicode_spaces(value) for key, value in data.items()}
+  elif isinstance(data, list):
+    return [replace_unicode_spaces(item) for item in data]
+  elif isinstance(data, str):
+    data = data.replace('\u202f', ' ')
+    data = data.replace('\u2009', ' ')
+    data = data.replace('\u2013', '-')
+    return data
+  else:
+    return data
 
 for county in indiana_counties:
     search_query = f"township trustee office {county}, Indiana"
@@ -47,13 +61,13 @@ for county in indiana_counties:
         # Get opening hours from Place Details
         if 'opening_hours' in place_details['result']:
             place_info["Hours"] = place_details['result']['opening_hours'].get('weekday_text', 'N/A')
-        else:
-            place_info["Hours"] = 'N/A'
 
+        # --- Clean up Unicode Spaces Before Saving ---
+        place_info = replace_unicode_spaces(place_info) 
         all_township_data.append(place_info)
 
 # Save data to a JSON file
-with open("indiana_township_trustees.json", "w") as f:
+with open("indiana_township_trustees.json", "w", encoding='utf-8') as f:
     json.dump(all_township_data, f, indent=4)
 
 print("Data saved to indiana_township_trustees.json")
