0f2cb5e8ec8ef8c016e5488bd90bf19e2619e60c
Alex Yong <alexjyong@gmail.com>
2025-01-29 20:50:20 +0000

    update

--- start_nominatim.sh
+++ start_nominatim.sh
@@ -1,6 +1,7 @@
 docker run -it \
   -e PBF_URL=https://download.geofabrik.de/north-america/us/indiana-latest.osm.pbf \
   -e REPLICATION_URL=https://download.geofabrik.de/north-america/us/indiana-updates/ \
+  -e IMPORT_TIGER_ADDRESSES=true \
   -p 8080:8080 \
   --name nominatim \
-  mediagis/nominatim:4.4 
+  mediagis/nominatim:4.5 
