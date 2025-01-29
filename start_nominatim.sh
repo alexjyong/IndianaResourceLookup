a157b95c1cb4b0c17f6204ca2a18a98f85b00ec2
Alex Yong <alexjyong@gmail.com>
2025-01-29 21:52:27 +0000

    stashing changes

--- start_nominatim.sh
+++ start_nominatim.sh
@@ -1,9 +1,9 @@
 docker run -it \
   -e PBF_URL=https://download.geofabrik.de/north-america/us/indiana-latest.osm.pbf \
   -e REPLICATION_URL=https://download.geofabrik.de/north-america/us/indiana-updates/ \
-  -e IMPORT_TIGER_ADDRESSES=/nominatim/data/indiana.tar.gz \
+  -e IMPORT_TIGER_ADDRESSES=/nominatim/tiger-nominatim-preprocessed.csv.tar.gz \
   -e IMPORT_US_POSTCODES=true \
   -p 8080:8080 \
-  -v /indiana_tiger:/nominatim/data \
+  -v /indiana_tiger:/nominatim/ \
   --name nominatim \
-  mediagis/nominatim:4.5 
+  mediagis/nominatim:4.5
